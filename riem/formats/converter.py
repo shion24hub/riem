98f03208df803887f10c311cb27ac49cb7290847
Shion Noguchi <shion24stat@gmail.com>
2024-02-17 01:04:08 +0900

    add abstractmethod formatting raw_data from db.

--- riem/formats/converter.py
+++ riem/formats/converter.py
@@ -20,6 +20,10 @@ class Converter(metaclass=ABCMeta):
     def format_from_bybit(self, raw_data: Any):
         pass
 
+    @abstractmethod
+    def format_from_db(self, raw_data: Any):
+        pass
+
     @property
     @abstractmethod
     def get_data_type(self) -> str:
